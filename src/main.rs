0e274a7ee115d29996f5b7d7c041ffbecfc1de51
Evan Walter <evan.walter.software@gmail.com>
2023-05-27 11:40:33 -0700

    Handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use ntex::web;
 
+mod error;
 mod services;
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
     web::server(|| {
-        web::App::new().default_service(web::route().to(services::default))
+        web::App::new()
+            // Default endpoint for unregisterd endpoints
+            .default_service(web::route().to(services::default))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
