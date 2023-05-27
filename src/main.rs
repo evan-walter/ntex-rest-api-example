5c43876600e0672e18235dacb5255fc924b8eb63
Evan Walter <evan.walter.software@gmail.com>
2023-05-27 11:29:47 -0700

    Start to add endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use ntex::web;
 
-#[web::get("/")]
-async fn index() -> &'static str {
-    "Hello world!"
-}
+mod services;
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
-    web::server(|| web::App::new().service(index))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await?;
+    web::server(|| {
+        web::App::new().default_service(web::route().to(services::default))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await?;
     Ok(())
 }
